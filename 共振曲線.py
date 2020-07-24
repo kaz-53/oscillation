284cd7d6193bc70b7a4e53f306413b10ee43b233
kaz-53 <54115486+kaz-53@users.noreply.github.com>
2020-07-24 15:33:26 +0900

    共振曲線を表示して保存するプログラム
    タイトル等の日本語が文字化けしないように編集しました

--- 共振曲線.py
+++ 共振曲線.py
@@ -5,7 +5,7 @@ def forced_oscillation(g):  # 引数は減衰比
     y = []  # プロット用データ(y軸)
     for t in range(0, 40000):  # プロットデータ作成
         t *= 0.01
-        w = t
+        w = t  # 入力の円振動数
         Xs = 1 / ((wn ** 2 - w ** 2) ** 2 + (2 * g * wn * w) ** 2) ** 0.5  # 振幅
         XsXst = Xs * wn ** 2  # 振幅倍率
         x.append(w / wn)  # x軸データの追加
@@ -24,10 +24,10 @@ forced_oscillation(0.1)  # 減衰比0.1の時のプロット作成
 forced_oscillation(0.2)  # 減衰比0.2の時のプロット作成
 forced_oscillation(0.5)  # 減衰比0.5の時のプロット作成
 
-plt.legend()  # 凡例表示
-plt.title("共振曲線")  # タイトル
-plt.xlabel("ω/ωn")  # x軸タイトル
-plt.ylabel("Xs/Xst")  # y軸タイトル
+plt.legend(prop={"family":"MS Gothic"})  # 凡例表示
+plt.title("共振曲線", fontname="MS Gothic")  # タイトル
+plt.xlabel("ω/ωn", fontname="MS Gothic")  # x軸タイトル
+plt.ylabel("Xs/Xst", fontname="MS Gothic")  # y軸タイトル
 plt.grid()  # グリッド線表示
 plt.savefig("共振曲線.png")  # 保存する
 plt.show()  # プロット表示
